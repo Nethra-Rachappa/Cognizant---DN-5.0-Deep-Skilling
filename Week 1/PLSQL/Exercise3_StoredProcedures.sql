934d8eb6f289923267305a62280701e214cae3a7
Nethra-Rachappa <nethrarachappa06@gmail.com>
2026-07-11 13:32:35 +0530

    Add stored procedures for interest, bonus, and transfers

--- Week 1/PLSQL/Exercise3_StoredProcedures.sql
+++ Week 1/PLSQL/Exercise3_StoredProcedures.sql
@@ -27,3 +27,102 @@ BEGIN
     add_numbers(10,20);
 END;
 /
+
+
+
+
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest
+
+AS
+
+BEGIN
+
+UPDATE Accounts
+
+SET Balance=Balance+(Balance*0.01)
+
+WHERE AccountType='Savings';
+
+COMMIT;
+
+END;
+/
+
+
+
+
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus(
+
+p_department VARCHAR2,
+
+p_bonus NUMBER
+
+)
+
+AS
+
+BEGIN
+
+UPDATE Employees
+
+SET Salary=Salary+(Salary*p_bonus/100)
+
+WHERE Department=p_department;
+
+COMMIT;
+
+END;
+/
+
+
+
+
+
+CREATE OR REPLACE PROCEDURE TransferFunds(
+
+p_from NUMBER,
+
+p_to NUMBER,
+
+p_amount NUMBER
+
+)
+
+AS
+
+v_balance NUMBER;
+
+BEGIN
+
+SELECT Balance
+
+INTO v_balance
+
+FROM Accounts
+
+WHERE AccountID=p_from;
+
+IF v_balance>=p_amount THEN
+
+UPDATE Accounts
+
+SET Balance=Balance-p_amount
+
+WHERE AccountID=p_from;
+
+UPDATE Accounts
+
+SET Balance=Balance+p_amount
+
+WHERE AccountID=p_to;
+
+COMMIT;
+
+ELSE
+
+DBMS_OUTPUT.PUT_LINE('Insufficient Balance');
+
+END IF;
+
+END;
+/
