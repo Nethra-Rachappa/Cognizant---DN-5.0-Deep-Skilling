fecc4812f5f574c5d044df3b0449b5a1ce49d53a
Nethra-Rachappa <nethrarachappa06@gmail.com>
2026-07-11 13:30:40 +0530

    Enhance customer loan interest and VIP status logic
    Added logic to update interest rates for customers over 60 and set VIP status based on balance.

--- Week 1/PLSQL/Exercise1_ControlStructures.sql
+++ Week 1/PLSQL/Exercise1_ControlStructures.sql
@@ -14,3 +14,61 @@ BEGIN
     END IF;
 END;
 /
+
+
+DECLARE
+    v_age NUMBER;
+BEGIN
+    FOR c IN (SELECT CustomerID, DOB FROM Customers) LOOP
+
+        v_age := FLOOR(MONTHS_BETWEEN(SYSDATE,c.DOB)/12);
+
+        IF v_age > 60 THEN
+
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = c.CustomerID;
+
+        END IF;
+
+    END LOOP;
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Interest updated successfully.');
+
+END;
+/
+
+
+ALTER TABLE Customers
+ADD IsVIP VARCHAR2(5);
+
+
+
+BEGIN
+
+FOR c IN (SELECT CustomerID,Balance FROM Customers) LOOP
+
+    IF c.Balance > 10000 THEN
+
+        UPDATE Customers
+        SET IsVIP='TRUE'
+        WHERE CustomerID=c.CustomerID;
+
+    ELSE
+
+        UPDATE Customers
+        SET IsVIP='FALSE'
+        WHERE CustomerID=c.CustomerID;
+
+    END IF;
+
+END LOOP;
+
+COMMIT;
+
+DBMS_OUTPUT.PUT_LINE('VIP Status Updated');
+
+END;
+/
