ac67b10045aa82ccb431c276c21cb8e42a3de385
Nethra-Rachappa <nethrarachappa06@gmail.com>
2026-07-26 14:51:17 +0530

    Add HelloMaven class with main method

---
+++ Week 2/Spring-Core-and-Maven/Exercise-4-Creating-and-Configuring-a-Maven-Project/HelloMaven.java
@@ -0,0 +1,9 @@
+package com.cognizant.maven;
+
+public class HelloMaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Maven!");
+        System.out.println("Maven Project Configured Successfully.");
+    }
+}
